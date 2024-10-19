e585c4be92a903fb35516cb03eb38013621c1343
Sigurd Smeby <sigurd.smeby@proton.me>
2024-10-19 14:20:06 +0200

    la inn degub i gitignore

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f545daae68081c9e49a2673a787cd56ac4266d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe55d0d959028b4d973725a8f383ef9828336cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
