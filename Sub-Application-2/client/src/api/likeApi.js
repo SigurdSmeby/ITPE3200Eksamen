feeebea658d3e5ab59aa8aa831dc24171f2467fd
Sindrekuljo <sindrekj97@hotmail.com>
2024-11-23 15:47:40 +0100

    fikset en feil ved oppdatering when login

--- Sub-Application-2/client/src/api/likeApi.js
+++ Sub-Application-2/client/src/api/likeApi.js
@@ -9,6 +9,19 @@ const axiosInstance = axios.create({
     },
 });
 
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 // Create a like
 export const createLike = async (postId) => {
     try {
