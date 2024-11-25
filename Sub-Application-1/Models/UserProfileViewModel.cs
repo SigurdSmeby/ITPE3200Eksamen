1e4971f9b1bbe6b01f9468f3b39eb99325eb132c
olaaxe <olaaxe@hotmail.com>
2024-11-25 13:21:29 +0100

    lagt inn settingslogikk
    mangler feilhåndtering og passorsbytte

--- Sub-Application-1/Models/UserProfileViewModel.cs
+++ Sub-Application-1/Models/UserProfileViewModel.cs
@@ -1,20 +1,13 @@
-namespace Sub_Application_1.DTOs
+public class UserProfileViewModel
 {
-    public class UserProfileDto
-    {
-    public string? UserId { get; set; }
     public string? Username { get; set; }
     public string? Email { get; set; }
     public string? ProfilePictureUrl { get; set; }
     public string? Bio { get; set; }
     public IFormFile? ProfilePicture { get; set; } // For file upload
 
-    public DateTime? DateJoined { get; set; }
-    public int? TotalPosts { get; set; }
-
     // For changing password
     public string? CurrentPassword { get; set; }
     public string? NewPassword { get; set; }
     public string? ConfirmPassword { get; set; }
-    }
 }
