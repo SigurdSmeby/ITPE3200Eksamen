b1590494198e072a966f4f173bfabbdc0bc79252
CarlHaHa <carl.hardhaug@gmail.com>
2024-11-30 18:58:03 +0100

    Fiksa subapp1 igjen :Hrek_Smile::

--- Sub-Application-1/Models/Post.cs
+++ Sub-Application-1/Models/Post.cs
@@ -8,21 +8,19 @@ namespace Sub_Application_1.Models
     {
         public int PostId { get; set; }
 
-        public required String UserId { get; set; } 
+        public required String UserId { get; set; }
 
-        public string? ImagePath { get; set; } 
+        public string? ImagePath { get; set; }
 
         [MaxLength(1000)]
-        public string? TextContent { get; set; } 
+        public string? TextContent { get; set; }
 
         public DateTime DateUploaded { get; set; } = DateTime.Now;
 
-        public int FontSize { get; set; } = 16; 
-        public string TextColor { get; set; } = "#000000"; 
-        public string BackgroundColor { get; set; } = "#FFFFFF"; 
-
-        // Navigation properties
-        public virtual UserUser { get; set; } = default!;
+        public int FontSize { get; set; } = 16;
+        public string TextColor { get; set; } = "#000000";
+        public string BackgroundColor { get; set; } = "#FFFFFF";
+        public virtual User User { get; set; } = default!;
         public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
         public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
     }
