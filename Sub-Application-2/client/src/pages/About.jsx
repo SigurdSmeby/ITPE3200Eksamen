054f9dbcf8a9f4afe8e1be1aa601c563d5dc847e
Sindrekuljo <sindrekj97@hotmail.com>
2024-12-01 23:42:41 +0100

    fixed css nacbar and footer fixed

--- Sub-Application-2/client/src/pages/About.jsx
+++ Sub-Application-2/client/src/pages/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
         <Container className="my-5">
             <h1>About Us</h1>
             <p>
-                Welcome to the About Page! Here you can learn more about our mission, values, and team.
-            </p>
-            <p>
-                We strive to provide the best service to our users, offering amazing features and support.
+            This is a web application project which is mainly about interacting and sharing pictures and notes with others. 
+            The target users include photographers, writers, artists, students, and anyone interested in documenting 
+            their thoughts and experiences through notes and images. The platform aims to foster a community where users 
+            can engage with each other's content through comments and discussions.
             </p>
         </Container>
     );
